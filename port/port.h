aa7e18219f4d80c2f31755adb582d809d765f9b8
Tarcísio Genaro Rodrigues <tarcisio.rodrigues@ortizgaming.com>
2014-05-23 15:12:05 -0300

    Added some function to port.h

--- port/port.h
+++ port/port.h
@@ -1,6 +1,12 @@
 #ifndef PG_PORT_H
 #define PG_PORT_H
 
-gettimeofday(struct timeval * tp, struct timezone * tzp);
+#include <stdint.h>
+
+int gettimeofday(struct timeval * tp, struct timezone * tzp);
+
+int pg_strcasecmp(const char *s1, const char *s2);
+
+int pg_strncasecmp(const char *s1, const char *s2, size_t n);
 
 #endif // PG_PROT_H
