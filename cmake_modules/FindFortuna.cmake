1631882b8b006555f5d1796352aef553c19b0835
Tarcísio Genaro Rodrigues <tarcisio.rodrigues@ortizgaming.com>
2014-05-26 20:08:19 -0300

    Find libfortuna linux windows

--- cmake_modules/FindFortuna.cmake
+++ cmake_modules/FindFortuna.cmake
@@ -1,12 +1,13 @@
-set (CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} $ENV{PROGRAMFILES}/fortuna)
-
 if (libfortuna_USE_STATIC_LIBS)
 	find_library (Fortuna_LIBRARIES fortuna-st)
 else ()
 	find_library (Fortuna_LIBRARIES fortuna)
 endif ()
 
-find_path (Fortuna_INCLUDE_DIRS NAMES fortuna.h PATH fortuna libfortuna)
+find_path (Fortuna_INCLUDE_DIRS
+	NAMES fortuna.h
+	PATHS /usr/include /usr/local/include ENV PROGRAMFILES
+	PATH_SUFFIXES fortuna libfortuna)
 
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (Fortuna DEFAULT_MSG Fortuna_LIBRARIES Fortuna_INCLUDE_DIRS)
