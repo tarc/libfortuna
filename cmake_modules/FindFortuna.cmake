19871c5af230fd58c0394f17cabbf7aa18c278f5
Tarcísio Genaro Rodrigues <tarcisio.rodrigues@ortizgaming.com>
2014-05-26 18:28:36 -0300

    Link to libfortuna shared lib.

--- cmake_modules/FindFortuna.cmake
+++ cmake_modules/FindFortuna.cmake
@@ -1,14 +1,12 @@
 set (CMAKE_PREFIX_PATH ${CMAKE_PREFIX_PATH} $ENV{PROGRAMFILES}/fortuna)
 
 if (libfortuna_USE_STATIC_LIBS)
+	find_library (Fortuna_LIBRARIES fortuna-st)
+else ()
 	find_library (Fortuna_LIBRARIES fortuna)
 endif ()
 
 find_path (Fortuna_INCLUDE_DIRS NAMES fortuna.h PATH fortuna libfortuna)
 
 include (FindPackageHandleStandardArgs)
-if (libfortuna_USE_STATIC_LIBS)
-	find_package_handle_standard_args (Fortuna DEFAULT_MSG Fortuna_LIBRARIES Fortuna_INCLUDE_DIRS)
-else ()
-	find_package_handle_standard_args (Fortuna DEFAULT_MSG Fortuna_INCLUDE_DIRS)
-endif ()
+find_package_handle_standard_args (Fortuna DEFAULT_MSG Fortuna_LIBRARIES Fortuna_INCLUDE_DIRS)
